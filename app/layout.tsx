66f8395cd776ff69a710125969f7e320de65fb31
Olumide Micheal <olumidemicheal7@gmail.com>
2024-09-29 14:38:01 +0100

    added opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import WalletContextProvider from "./context/walletProvider";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = localFont({
@@ -18,7 +19,17 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Velox Finanace",
   description:
-    "High-speed token exchange platform on Solana, utilizing ZK compression to deliver fast, cost-effective transactions.",
+    "A token exchange platform on Solana, utilizing ZK compression to deliver fast, cost-effective transactions.",
+  openGraph: {
+    images: "/opengraph-image.png",
+  },
+  twitter: {
+    title: "Velox Finanace",
+    description:
+      "A token exchange platform on Solana, utilizing ZK compression to deliver fast, cost-effective transactions.",
+    images: "/twitter-image.png",
+    creator: "@codewithmide",
+  },
 };
 
 export default function RootLayout({
@@ -34,6 +45,7 @@ export default function RootLayout({
         <WalletContextProvider>
           {children}
           <Toaster />
+          <Analytics />
         </WalletContextProvider>
       </body>
     </html>
