5b97fec66d300c04325edf09be0021ccaf8e7bb2
Olumide Micheal <olumidemicheal7@gmail.com>
2024-10-21 20:25:42 +0100

    fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="center flex-col min-h-screen p-3 md:p-6 md:pb-[16rem] pb-[10rem] gap-16">
       <Navbar />
       <div className="flex-col center xl:w-[60%] md:w-[90%] gap-6">
-        <h1 className="lg:text-[72px] md:text-[60px] text-[38px] text-center leading-none font-bold gradient-text">
+        <h1 className="lg:text-[72px] md:text-[60px] text-[32px] text-center leading-none font-bold gradient-text">
           Navigate Solana&apos;s Compressed Assets
         </h1>
         <p className="text-center md:w-[60%] w-[90%] mb-[4rem]">
