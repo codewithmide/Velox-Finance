9f17a86b6afc7f82a05c099a5d6f55fa83f7cc20
Olumide Micheal <olumidemicheal7@gmail.com>
2024-10-21 18:34:45 +0100

    fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <h1 className="lg:text-[72px] md:text-[60px] text-[30px] text-center leading-none font-bold gradient-text">
           Navigate Solana&apos;s Compressed Assets
         </h1>
-        <p className="text-center md:w-[80%] w-[90%] mb-[4rem]">
+        <p className="text-center md:w-[60%] w-[90%] mb-[4rem]">
           A powerful tool that finds and indexes compressed tokens across
           the Solana network, making them easier to discover and manage.
         </p>
