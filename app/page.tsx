1a812909834ce1151954f4f3c0ec62f0316e383a
Olumide Micheal <olumidemicheal7@gmail.com>
2024-10-21 18:27:09 +0100

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Navbar />
       <div className="flex-col center xl:w-[48%] md:w-[90%] gap-6">
         <h1 className="lg:text-[72px] md:text-[60px] text-[30px] text-center leading-none font-bold gradient-text">
-          Navigate Solana's Compressed Assets
+          Navigate Solana&apos;s Compressed Assets
         </h1>
         <p className="text-center md:w-[80%] w-[90%] mb-[4rem]">
           A powerful tool that finds and indexes compressed tokens across
